32659700d725b36a3dcd303180f51f56c63e105b
unknown <kalisch@M3.ict.usc.edu>
2015-09-14 11:21:33 -0700

    fix to avoid external entity injection when handling PML;

--- src/edu/usc/ict/nl/bus/protocols/VHPMLProtocol.java
+++ src/edu/usc/ict/nl/bus/protocols/VHPMLProtocol.java
@@ -1,9 +1,18 @@
 package edu.usc.ict.nl.bus.protocols;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 import edu.usc.ict.nl.bus.NLBus;
 import edu.usc.ict.nl.pml.PMLStateKeeper;
@@ -16,6 +25,7 @@ import edu.usc.ict.vhmsg.MessageListener;
 public class VHPMLProtocol extends Protocol {
 	// PML processing
 	private Unmarshaller pmlParser;
+	private XMLReader xmlReader;
 	private PMLStateKeeper pmlStateKeeper=null;
 	private VHBridge vhBridge=null;
 
@@ -27,6 +37,19 @@ public class VHPMLProtocol extends Protocol {
 			pmlParser = jc.createUnmarshaller();
 			pmlStateKeeper=new PMLStateKeeper(bus);
 			vhBridge.addMessageListenerFor("vrPerception", createVrPerceptionMessageListener());
+			
+	        SAXParserFactory spf = SAXParserFactory.newInstance();
+	        spf.setValidating(true);
+	        spf.setXIncludeAware(false);
+	        spf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+	        xmlReader = spf.newSAXParser().getXMLReader();
+	        xmlReader.setEntityResolver(new EntityResolver() {
+				
+				@Override
+				public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+					return new InputSource(); // do not allow external entities
+				}
+			});
 		} else {
 			logger.error(this.getClass()+" requested to start but no VH/PML configuration.");
 		}
@@ -60,8 +83,10 @@ public class VHPMLProtocol extends Protocol {
 	 * @throws Exception
 	 */
 	public void handlePerceptionEvent(VRPerception msg) throws Exception {
-		ByteArrayInputStream input = new ByteArrayInputStream (msg.getPerceptionEvent().getBytes());
-		Pml pml = (Pml)pmlParser.unmarshal(input);
+		ByteArrayInputStream byteStream = new ByteArrayInputStream (msg.getPerceptionEvent().getBytes());
+        InputSource inputSource = new InputSource(byteStream);
+        SAXSource source = new SAXSource(xmlReader, inputSource);
+		Pml pml = (Pml)pmlParser.unmarshal(source);
 		pmlStateKeeper.add(pml);
 	}
 	@Override
